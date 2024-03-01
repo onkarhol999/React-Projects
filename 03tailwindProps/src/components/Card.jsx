1890390404bfddf90aae51760f575f7f45f7408c
Onkar hol <holonkar2003@gmail.com>
2024-03-01 15:22:35 +0530

    Password Generator

--- 03tailwindProps/src/components/Card.jsx
+++ 03tailwindProps/src/components/Card.jsx
@@ -4,7 +4,7 @@ function Card(props){
     console.log(props);
   return(
     <div className="relative h-[400px] w-[300px] rounded-md">
-  <img
+  <img 
     src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
     alt="AirMax Pro"
     className="z-0 h-full w-full rounded-md object-cover"
